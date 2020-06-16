1073b7c0a93879ed959ac2f884ff819d8ee371b3
baryska <55710842+baryska@users.noreply.github.com>
2020-06-16 12:22:15 +0200

    header and footer added

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="container">
-    <router-link to="/page1">Go to Page 1</router-link>
-    <router-link to="/page2">Go to Page 2</router-link>
-    <router-link to="/profiles">Go to Profiles</router-link>
-    <router-link to="/about">O projektu</router-link>
+  <div class="hlavicka">
+    <header>
+      <img src="assets/img/logo.png" alt="logo" />
+      <router-link to="/page1">Go to Page 1</router-link>
+      <router-link to="/page2">Go to Page 2</router-link>
+      <router-link to="/profiles">Go to Profiles</router-link>
+      <router-link to="/about">O projektu</router-link>
+      <router-link to="/">Domů</router-link>
+    </header>
+    <img class="d-none d-lg-block d-xl-block" src="assets/img/jumbotron_desktop.png" />
+    <img class="d-none d-md-block d-lg-none" src="assets/img/jumbotron_tablet.png" />
+    <img class="d-xs-block d-md-none" src="assets/img/jumbotron_mobile.png" />
 
     <h1>Vue router</h1>
     <div>
@@ -18,6 +25,9 @@
       <b-modal id="modal-1" title="BootstrapVue">
         <p class="my-4">Hello from modal!</p>
       </b-modal>
+      <img class="d-none d-md-block footer" src="assets/img/footer.png" />
+      <img class="d-none d-md-block d-lg-none footer" src="assets/img/footer_tablet.png" />
+      <img class="d-xs-block d-md-none footer" src="assets/img/footer_mobile.png" />
     </div>
 
     <router-view></router-view>
@@ -37,5 +47,45 @@ export default {
 
 html {
   font-family: sans-serif;
+}
+
+body img {
+  width: 100%;
+}
+
+.footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+
+@media (max-width: 767.98px) {
+  header {
+    height: 90px;
+  }
+  header img {
+    width: 200px;
+    padding: 30px 20px;
+  }
+}
+
+@media (max-width: 991.98px) and (min-width: 767.99px) {
+  header {
+    height: 110px;
+  }
+  header img {
+    width: 250px;
+    padding: 30px 20px;
+  }
+}
+
+@media (min-width: 991.99px) {
+  header {
+    height: 120px;
+  }
+  header img {
+    width: 300px;
+    padding: 30px 20px;
+  }
 }
 </style>
