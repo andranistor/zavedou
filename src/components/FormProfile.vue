9d553cf8b80ff971899038ba74fdaf8fc4ab636e
baryska <55710842+baryska@users.noreply.github.com>
2020-06-22 13:11:00 +0200

    FormProfile and packaage json changed

--- src/components/FormProfile.vue
+++ src/components/FormProfile.vue
@@ -1,29 +1,30 @@
 <template>
   <div>
     <h2>Formulář</h2>
-    <form @submit.prevent="addProfile">
-      <label for="first-name">
-        Jméno:
-        <input
-          type="text"
-          id="first-name"
-          v-model.trim="firstName"
-          placeholder="Jméno"
-          required
-        />
-      </label>
-      <label for="last-name">
-        Příjmení:
-        <input
-          type="text"
-          id="last-name"
-          v-model.trim="lastName"
-          placeholder="Příjmení"
-          required
-        />
-      </label>
-      <br />
-      <!-- <label for="branch-select">
+    <div class="form">
+      <form @submit.prevent="addProfile">
+        <label for="first-name">
+          Jméno:
+          <input
+            type="text"
+            id="first-name"
+            v-model.trim="firstName"
+            placeholder="Jméno"
+            required
+          />
+        </label>
+        <label for="last-name">
+          Příjmení:
+          <input
+            type="text"
+            id="last-name"
+            v-model.trim="lastName"
+            placeholder="Příjmení"
+            required
+          />
+        </label>
+        <br />
+        <!-- <label for="branch-select">
         Vyberte obor:
         <select name="branch" id="branch-select" v-model="branch">
           <option>Matematika, fyzika a informatika</option>
@@ -35,121 +36,132 @@
           <option>Společenské a humanitní vědy</option>
         </select>
       </label>
-      <br />-->
-      <label for="field">
-        Specifikujte váš obor:
-        <input
-          type="text"
-          id="field"
-          v-model.trim="field"
-          placeholder="Obor"
-          required
-        />
-      </label>
-      <br />
-      <label for="subject-select">
-        Vhodné pro školní předmět:
-        <select
-          name="type"
-          id="subject-select"
-          v-model="subject"
-          required
-        >
-          <option>Cizí jazyky</option>
-          <option>Čeština</option>
-          <option>Dějepis</option>
-          <option>Fyzika</option>
-          <option>Chemie</option>
-          <option>Matematika</option>
-          <option>Občanská výchova</option>
-          <option>Přirodopis/biologie</option>
-          <option>Informatika</option>
-          <option>Hudební a výtvarná výchova</option>
-          <option>Zeměpis</option>
-        </select>
-      </label>
-      <br />
-      <label for="institution">
-        Instituce:
-        <input
-          type="text"
-          id="institution"
-          v-model.trim="institution"
-          placeholder="Instituce"
-          required
-        />
-      </label>
-      <br />
-      <label for="address">
-        Adresa instituce:
-        <input
-          type="text"
-          id="address"
-          v-model.trim="address"
-          placeholder="Adresa"
-          required
-        />
-      </label>
-      <br />
-      <label for="offer">
-        Nabízím:
-        <textarea
-          id="offer"
-          v-model.trim="offer"
-          placeholder="Vysvětlím teorii relativity"
-          required
-        ></textarea>
-      </label>
-      <br />
-      <label for="whom-select">
-        Pro koho:
-        <select name="type" id="whom-select" v-model="whom" required>
-          <option>Všechny</option>
-          <option>1. stupeň ZŠ</option>
-          <option>2. stupeň ZŠ</option>
-          <option>Střední školy</option>
-          <option>Dospělé</option>
-        </select>
-      </label>
-      <br />
-      <label for="region-select">
-        Pro oblast:
-        <select name="type" id="region-select" v-model="region" required>
-          <option>Celá ČR</option>
-          <option>Hlavní město Praha</option>
-          <option>Středočeský kraj</option>
-          <option>Jihočeský kraj</option>
-          <option>Plzeňský kraj</option>
-          <option>Karlovarský kraj</option>
-          <option>Ústecký kraj</option>
-          <option>Liberecký kraj</option>
-          <option>Královéhradecký kraj</option>
-          <option>Pardubický kraj</option>
-          <option>Kraj Vysočina</option>
-          <option>Jihomoravský kraj</option>
-          <option>Olomoucký kraj</option>
-          <option>Zlínský kraj</option>
-          <option>Moravskoslezský kraj</option>
-        </select>
-      </label>
-      <br />
-      <label for="contact">
-        E-mail:
-        <input type="email" id="contact" v-model.trim="contact" placeholder="Email" required />
-      </label>
-      <br />
+        <br />-->
+        <label for="field">
+          Specifikujte váš obor:
+          <input
+            type="text"
+            id="field"
+            v-model.trim="field"
+            placeholder="Obor"
+            required
+          />
+        </label>
+        <br />
+        <label for="subject-select">
+          Vhodné pro školní předmět:
+          <select
+            name="type"
+            id="subject-select"
+            v-model="subject"
+            required
+          >
+            <option>Cizí jazyky</option>
+            <option>Čeština</option>
+            <option>Dějepis</option>
+            <option>Fyzika</option>
+            <option>Chemie</option>
+            <option>Matematika</option>
+            <option>Občanská výchova</option>
+            <option>Přirodopis/biologie</option>
+            <option>Informatika</option>
+            <option>Hudební a výtvarná výchova</option>
+            <option>Zeměpis</option>
+          </select>
+        </label>
+        <br />
+        <label for="institution">
+          Instituce:
+          <input
+            type="text"
+            id="institution"
+            v-model.trim="institution"
+            placeholder="Instituce"
+            required
+          />
+        </label>
+        <br />
+        <label for="address">
+          Adresa instituce:
+          <input
+            type="text"
+            id="address"
+            v-model.trim="address"
+            placeholder="Adresa"
+            required
+          />
+        </label>
+        <br />
+        <label for="offer">
+          Nabízím:
+          <textarea
+            id="offer"
+            v-model.trim="offer"
+            placeholder="Vysvětlím teorii relativity"
+            required
+          ></textarea>
+        </label>
+        <br />
+        <label for="whom-select">
+          Pro koho:
+          <select name="type" id="whom-select" v-model="whom" required>
+            <option>Všechny</option>
+            <option>1. stupeň ZŠ</option>
+            <option>2. stupeň ZŠ</option>
+            <option>Střední školy</option>
+            <option>Dospělé</option>
+          </select>
+        </label>
+        <br />
+        <label for="region-select">
+          Pro oblast:
+          <select name="type" id="region-select" v-model="region" required>
+            <option>Celá ČR</option>
+            <option>Hlavní město Praha</option>
+            <option>Středočeský kraj</option>
+            <option>Jihočeský kraj</option>
+            <option>Plzeňský kraj</option>
+            <option>Karlovarský kraj</option>
+            <option>Ústecký kraj</option>
+            <option>Liberecký kraj</option>
+            <option>Královéhradecký kraj</option>
+            <option>Pardubický kraj</option>
+            <option>Kraj Vysočina</option>
+            <option>Jihomoravský kraj</option>
+            <option>Olomoucký kraj</option>
+            <option>Zlínský kraj</option>
+            <option>Moravskoslezský kraj</option>
+          </select>
+        </label>
+        <br />
+        <label for="contact">
+          E-mail:
+          <input
+            type="email"
+            id="contact"
+            v-model.trim="contact"
+            placeholder="Email"
+            required
+          />
+        </label>
+        <br />
 
-      <!-- <p v-if="errors.length">Please correct the following error(s):</p>
+        <!-- <p v-if="errors.length">Please correct the following error(s):</p>
       <ul>
         <li v-for="error in errors">{{ error }}</li>
-      </ul>-->
+        </ul>-->
 
-      <button type="submit" v-on:submit.prevent="addProfile">Nahrajte profil</button>
-    </form>
-    <br />
-    <router-link to="/profiles">
-      <button>Zpět na profily</button>
-    </router-link>
+        <b-button
+          variant="outline-primary"
+          type="submit"
+          v-on:submit.prevent="addProfile"
+        >Nahrajte profil</b-button>
+      </form>
+      <br />
+      <router-link to="/profiles">
+        <b-button variant="outline-primary">Zpět na profily</b-button>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -241,3 +253,39 @@ export default {
   }
 };
 </script>
+<style>
+input[type="text"],
+select,
+textarea {
+  width: 100%; /* Full width */
+  padding: 12px; /* Some padding */
+  border: 1px solid #ccc; /* Gray border */
+  border-radius: 4px; /* Rounded borders */
+  box-sizing: border-box; /* Make sure that padding and width stays in place */
+  margin-top: 6px; /* Add a top margin */
+  margin-bottom: 16px; /* Bottom margin */
+  resize: vertical; /* Allow the user to vertically resize the textarea (not horizontally) */
+}
+
+/* Style the submit button with a specific background color etc */
+input[type="submit"] {
+  background-color: #4caf50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+/* When moving the mouse over the submit button, add a darker green color */
+input[type="submit"]:hover {
+  background-color: #45a049;
+}
+
+/* Add a background color and some padding around the form */
+.form {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
